4efc4c6597ae93be430b93f9e9c3ebc4176bf97d
Kiana Tennyson <kiana.tennyson@manheim.com>
2017-02-07 14:30:59 -0500

    adjustments to server.js

--- server.js
+++ server.js
@@ -1,15 +1,17 @@
 var AlexaAppServer = require('alexa-app-server');
 
 AlexaAppServer.start({
-  server_root: './',
+  server_root: __dirname,
+  server_dir: 'server',
+  app_root: '/alexa/',
+  log: true,
+  httpsEnabled: true,
+  httpsPort: 443,
   port: process.env.PORT || 3000,
-  // Use preRequest to load user data on each request and add it to the request json.
-  // In reality, this data would come from a db or files, etc.
   preRequest: function(json, req, res) {
     console.log("preRequest fired");
     json.userDetails = { "name": "Kiana" };
   },
-  // Add a dummy attribute to the response
   postRequest: function(json, req, res) {
     json.dummy = "text";
   }
