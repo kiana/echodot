3ebcc682dcdc03f68537ec5b73be008486de1e24
Kiana Tennyson <kiana.tennyson@manheim.com>
2017-02-07 14:19:22 -0500

    converting to alexa-app-server from express server

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
-var express = require('express')
-var app = express()
+var AlexaAppServer = require('alexa-app-server');
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
-})
-
-app.listen(process.env.PORT || 3000, function () {
-  console.log('Example app listening on port', process.env.PORT)
-})
+AlexaAppServer.start({
+  server_root: './',
+  port: 8080,
+  // Use preRequest to load user data on each request and add it to the request json.
+  // In reality, this data would come from a db or files, etc.
+  preRequest: function(json, req, res) {
+    console.log("preRequest fired");
+    json.userDetails = { "name": "Kiana" };
+  },
+  // Add a dummy attribute to the response
+  postRequest: function(json, req, res) {
+    json.dummy = "text";
+  }
+});
