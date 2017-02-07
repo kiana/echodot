89ef729831836b768ed451412585c3cd10cad155
Kiana Tennyson <kiana.tennyson@manheim.com>
2017-02-07 14:22:36 -0500

    oops forgot to do the process.env.PORT thing

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var AlexaAppServer = require('alexa-app-server');
 
 AlexaAppServer.start({
   server_root: './',
-  port: 8080,
+  port: process.env.PORT || 3000,
   // Use preRequest to load user data on each request and add it to the request json.
   // In reality, this data would come from a db or files, etc.
   preRequest: function(json, req, res) {
