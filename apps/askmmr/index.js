57a5936a0a71d9ffab849be36568f981effa31f4
Kiana Tennyson <kiana.tennyson@manheim.com>
2017-02-08 07:56:32 -0500

    adding fake, stubbed responses for mmr data

--- apps/askmmr/index.js
+++ apps/askmmr/index.js
@@ -1,6 +1,7 @@
 var alexa = require('alexa-app');
 
 module.change_code = 1;
+var mmr = '<say-as interpret-as="spell-out">MMR</say-as>';
 
 var app = new alexa.app('askmmr');
 app.dictionary = {"colors":["beige","black","blue","brown","burgundy","camouflage","charcoal","gold","gray","green","lime","off-white","orange","pink","purple","red","silver","turquoise","white","yellow"]};
@@ -20,7 +21,7 @@ app.intent('ForTransactions', {
     "for vin {one G two Z G five five eight seven six four two four nine eight seven zero|VIN}"
   ]
 }, function(req, res) {
-  res.say('yay for transactions');
+  res.say('Do you really want me to read out all those transactions? I don\'t think so.');
 });
 
 app.intent('ForHistoricalAverage', {
@@ -34,7 +35,13 @@ app.intent('ForHistoricalAverage', {
     "for vin {one G two Z G five five eight seven six four two four nine eight seven zero|VIN}"
   ]
 }, function(req, res) {
-  res.say('yay for historical average');
+  if (req.slot('VIN')) {
+    res.say('The historical average for the past 30 days is $1725, average odometer <say-as interpret-as="cardinal">143668</say-as>, 6 months ago is $1875, average odometer <say-as interpret-as="cardinal">136056</say-as>, last year is $2350, odometer <say-as interpret-as="cardinal">136715</say-as> for ' + req.slot('VIN'));
+  } else if (req.slot('MMS')){
+    res.say('The historical average for the past 30 days is $1725, average odometer <say-as interpret-as="cardinal">143668</say-as>, 6 months ago is $1875, average odometer <say-as interpret-as="cardinal">136056</say-as>, last year is $2350, odometer <say-as interpret-as="cardinal">136715</say-as> for ' + req.slot('YEAR') + req.slot('MMS'));
+  } else {
+    res.say('Unable to retrieve historical average at this time.');
+  }
 });
 
 app.intent('ForProjectedAverage', {
@@ -48,7 +55,13 @@ app.intent('ForProjectedAverage', {
     "for vin {one G two Z G five five eight seven six four two four nine eight seven zero|VIN}"
   ]
 }, function(req, res) {
-  res.say('yay for projected average');
+  if (req.slot('VIN')) {
+    res.say('The projected average next month is $1750 for ' + req.slot('VIN'));
+  } else if (req.slot('MMS')){
+    res.say('The projected average next month is $1750 for ' + req.slot('YEAR') + req.slot('MMS'));
+  } else {
+    res.say('Unable to retrieve projected average at this time.');
+  }
 });
 
 app.intent('ForEstimatedRetailValue', {
@@ -62,7 +75,13 @@ app.intent('ForEstimatedRetailValue', {
     "for vin {one G two Z G five five eight seven six four two four nine eight seven zero|VIN}"
   ]
 }, function(req, res) {
-  res.say('yay for estimated retail value');
+  if (req.slot('VIN')) {
+    res.say('Let\'s see<break time="1s" /> Today the estimated retail value for ' + req.slot('VIN') + ' is $5,175, with a typical range of $4,250 and $6,100');
+  } else if (req.slot('MMS')){
+    res.say('Let\'s see<break time="1s" /> Today the estimated retail value for ' + req.slot('YEAR') + req.slot('MMS') + ' is $5,175, with a typical range of $4,250 and $6,100');
+  } else {
+    res.say('Unable to retrieve estimated retail value at this time.');
+  }
 });
 
 app.intent('ToAdjustMMR', {
@@ -79,7 +98,21 @@ app.intent('ToAdjustMMR', {
     "by region {-|region}"
   ]
 }, function(req, res) {
-  res.say('yay to adjust mmr');
+  if (req.slot('region')) {
+    res.say('Checking the ' + req.slot('region') + ' region, the Adjusted ' +mmr+ ' drops $20 to $1750.');
+  }
+
+  if (req.slot('color')) {
+    res.say('Checking the color ' + req.slot('color')+ ', the Adjusted ' +mmr+ ' drops $90 to $1675.')
+  }
+
+  if (req.slot('condition')) {
+    res.say('Checking for condition ' + req.slot('condition') + ', the adjusted ' + mmr + ' increases $350 to $2,125.')
+  }
+
+  if (req.slot('miles')) {
+    res.say('With ' + req.slot('miles') + ' miles, the adjusted ' + mmr + ' increases $1560 to $3325. You should probably buy that car.');
+  }
 });
 
 app.intent('ForAverageMMR', {
@@ -93,7 +126,13 @@ app.intent('ForAverageMMR', {
     "for vin {one G two Z G five five eight seven six four two four nine eight seven zero|VIN}"
   ]
 }, function(req, res) {
-  res.say('yay for average mmr');
+  if (req.slot('VIN')) {
+    res.say('Let\'s see<break time="1s" /> Today the average <say-as interpret-as="spell-out">MMR</say-as> for ' + req.slot('VIN') + 'is $1,775, with average odometer of 143,668, average condition of 2 point zero, and a typical range between $1,000 and $2,5550');
+  } else if (req.slot('MMS')){
+    res.say('Let\'s see<break time="1s" /> Today the average <say-as interpret-as="spell-out">MMR</say-as> for ' + req.slot('YEAR') + req.slot('MMS') + 'is $1,775, with average odometer of 143,668, average condition of 2 point zero, and a typical range between $1,000 and $2,5550');
+  } else {
+    res.say('Unable to retrieve Average <say-as interpret-as="spell-out">MMR</say-as> at this time.');
+  }
 });
 
 module.exports = app;
